903c0f480b9d2662c37f940620ecc24a1f0bc587
shotta <erwan.gdc49@gmail.com>
2026-03-03 13:19:48 +0100

    import parse_config() function

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,7 +1,14 @@
 import sys
+from src.parse_config import parse_config
 
 
 def main() -> None:
     if len(sys.argv) != 2:
         print("Usage: python a_maze_ing.py <input_file>")
         sys.exit(1)
+
+    config = parse_config(sys.argv[1])
+
+
+if __name__ == "__main__":
+    main()
